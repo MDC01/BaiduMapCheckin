5900dbe84131f4fc64b9be272ed3c2777d4e97d6
MDC <17759711186@163.com>
2018-08-17 17:05:05 +0800

    commit to github

---
+++ app/src/main/java/com/example/alain/lbstest/DistanceUtil.java
@@ -0,0 +1,24 @@
+package com.example.alain.lbstest;
+
+import com.baidu.mapapi.model.CoordUtil;
+import com.baidu.mapapi.model.LatLng;
+import com.baidu.mapapi.model.inner.Point;
+
+/**
+ * Created by Alain on 2018/8/8.
+ */
+
+public class DistanceUtil {
+    public DistanceUtil() {
+    }
+
+    public static double getDistance(LatLng var0, LatLng var1) {
+        if(var0 != null && var1 != null) {
+            Point var2 = CoordUtil.ll2point(var0);
+            Point var3 = CoordUtil.ll2point(var1);
+            return var2 != null && var3 != null?CoordUtil.getDistance(var2, var3):-1.0D;
+        } else {
+            return -1.0D;
+        }
+    }
+}
